2787e12343baebe7c7f3c2927af3b406037563e8
ayushshirke123 <ayushshirke123@gmail.com>
2025-10-10 01:55:50 +0530

    Fix backend URL to aptitude-game-backend1.onrender.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://aptitude-game-backend.onrender.com',
-  socketUrl: 'https://aptitude-game-backend.onrender.com'
+  apiUrl: 'https://aptitude-game-backend1.onrender.com',
+  socketUrl: 'https://aptitude-game-backend1.onrender.com'
 };
